edeb1fadeaa9231ae4ee7d1c36ec82d898d72206
omesb <36600384+omesb@users.noreply.github.com>
2020-12-22 19:17:48 +0100

    build script fix

--- src/build.js
+++ src/build.js
@@ -1,29 +1,23 @@
 const fs = require('fs');
 const mustache = require('mustache');
 
-const manifest = require('../package.json');
-const template = JSON.stringify(require(`./template.json`));
-const colorSchemes = fs.readdirSync('./color-schemes').map((file) => {
+const manifest = require(`${__dirname}/../package.json`);
+const template = JSON.stringify(require(`${__dirname}/template.json`));
+const colorSchemes = fs.readdirSync(`${__dirname}/color-schemes`).map((file) => {
     return {
         filename: file.split('.json')[0].toLowerCase(),
-        scheme: require(`./color-schemes/${file}`),
+        scheme: require(`${__dirname}/color-schemes/${file}`),
     };
 });
 
 createThemes();
 
-// for (themeName of themeNames) {
-//   const content = require(`${__dirname}/${themeName}.json`);
-//   const theme = mustache.render(template, { ...content, ...globals });
-//   fs.writeFileSync(`${__dirname}/../../themes/${themeName}.json`, theme);
-// }
-
 function createThemes() {
     const manifestThemes = [];
     colorSchemes.forEach((schemeJSON) => {
         const theme = JSON.parse(mustache.render(template, schemeJSON.scheme));
-        theme.colors = require(`./${schemeJSON.scheme.type}.json`);
-        fs.writeFileSync(`../themes/${schemeJSON.filename}-color-theme.json`, JSON.stringify(theme, null, 4));
+        theme.colors = require(`${__dirname}/${schemeJSON.scheme.type}.json`);
+        fs.writeFileSync(`${__dirname}/../themes/${schemeJSON.filename}-color-theme.json`, JSON.stringify(theme, null, 4));
         manifestThemes.push({
             label: schemeJSON.scheme.name,
             uiTheme: schemeJSON.scheme.type == 'dark' ? 'vs-dark' : 'vs',
@@ -31,7 +25,7 @@ function createThemes() {
         });
     });
     manifest.contributes.themes = manifestThemes;
-    fs.writeFileSync(`../package.json`, JSON.stringify(manifest, null, 4))
+    fs.writeFileSync(`${__dirname}/../package.json`, JSON.stringify(manifest, null, 4))
 }
 
 function updateManifest(params) {}
